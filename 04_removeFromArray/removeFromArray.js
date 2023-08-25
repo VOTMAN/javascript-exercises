99f12a4267e380611a9d218cf3a3736f8913272a
Arnav <76613041+VOTMAN@users.noreply.github.com>
2023-08-25 18:22:28 +0530

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...num) {
+    let newArr = []
+    for (item of arr) {
+        if (!num.includes(item)) {
+            newArr.push(item);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
