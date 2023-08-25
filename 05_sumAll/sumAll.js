2ce6b419ed050d22e21dec707724ddf81818070b
Arnav <76613041+VOTMAN@users.noreply.github.com>
2023-08-25 18:54:09 +0530

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalNum = 0;
+    if (num1 === Number(num1) && num2 === Number(num2)) {
+        if (num1 < 0 || num2 < 0) {
+            return "ERROR"
+        } else {
+            if (num1 < num2) {
+                for (num1; num1 <= num2; num1++) {
+                    finalNum += num1
+                }
+            } else {
+                for (num2; num2 <= num1; num2++) {
+                    finalNum += num2;
+                }
+            }
+            return finalNum;
+        }
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
